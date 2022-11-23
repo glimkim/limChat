f6f29027c4d1ab64468ff8766941aad1d2b67ed5
glimkim <limkimdev@gmail.com>
2022-11-23 13:25:01 +0000

    fix: match port

--- app.js
+++ app.js
@@ -10,30 +10,32 @@ const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, 'src')));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, () => {console.log(`Server is running at ${PORT}`)});
+server.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
 
-io.on('connection', (socket) => { 
-    socket.on('inAlert', (data) => {
-        const { name } = data;
-        io.emit('inAlert', {
-            name
-        });
-    }); 
-    socket.on('profile', (data) => {
-        const { img } = data;
-        io.emit('profile', {
-            img
-        });
-    }); 
-    socket.on('chatting', (data) => {
-        const { name, msg, img } = data;
-        io.emit('chatting', {
-            name,
-            msg,
-            img,
-            time: moment(new Date()).tz("Asia/Seoul").format('h:mm A')
-        });
-    }); 
+io.on('connection', (socket) => {
+  socket.on('inAlert', (data) => {
+    const { name } = data;
+    io.emit('inAlert', {
+      name,
+    });
+  });
+  socket.on('profile', (data) => {
+    const { img } = data;
+    io.emit('profile', {
+      img,
+    });
+  });
+  socket.on('chatting', (data) => {
+    const { name, msg, img } = data;
+    io.emit('chatting', {
+      name,
+      msg,
+      img,
+      time: moment(new Date()).tz('Asia/Seoul').format('h:mm A'),
+    });
+  });
 });
